81754d7790003b3e336c506d68e6729033dd9f4e
wang0964 <wang0964@algonquinlive.com>
2025-06-23 19:50:05 -0400

    all

--- streamlit.py
+++ streamlit.py
@@ -7,9 +7,8 @@ import streamlit as st
 # Set the page title and description
 st.title("Credit Loan Eligibility Predictor")
 st.write("""
-This app predicts whether a loan applicant is eligible for a loan 
-based on various personal and financial characteristics.
-""")
+            This application predicts whether someone may have heart disease.
+        """)
 
 # Load the pre-trained model
 rf_pickle = open(r'models/model.pkl', 'rb')
@@ -22,8 +21,9 @@ with st.form("user_inputs"):
     st.subheader("Health Information")
     
     bmi = st.number_input("Body Mass Index", 
-                                    min_value=0, 
-                                    step=0.1)
+                                    min_value=0.0, 
+                                    step=0.1, 
+                                    value=20.0)
     
     smoking = st.selectbox("Smoking", options=["Yes", "No"])
      
@@ -32,12 +32,14 @@ with st.form("user_inputs"):
     stroke = st.selectbox("Stroke", options=["Yes", "No"])
     
     physical_health = st.number_input("PhysicalHealth", 
-                                    min_value=0, 
-                                    step=0.1)
+                                    min_value=0.0, 
+                                    step=0.1, 
+                                    value=5.0)
     
     mental_health = st.number_input("MentalHealth", 
-                                    min_value=0, 
-                                    step=0.1)        
+                                    min_value=0.0, 
+                                    step=0.1, 
+                                    value=5.0)        
      
     diff_walking = st.selectbox("DiffWalking", options=["Yes", "No"])
     
@@ -54,8 +56,9 @@ with st.form("user_inputs"):
     genHealth = st.selectbox("GenHealth", options=['Poor', 'Fair', 'Good', 'Very good', 'Excellent'])
     
     sleep_time = st.number_input("SleepTime", 
-                                    min_value=0, 
-                                    step=0.5)    
+                                    min_value=0.0, 
+                                    step=0.5, 
+                                    value=8.0)    
     
     asthma 	= st.selectbox("Asthma", options=["Yes", "No"])
     
@@ -69,48 +72,48 @@ with st.form("user_inputs"):
 
 # Handle the dummy variables to pass to the model
 if submitted:
-    smoking = 1 if smoking=='Yes' else 0
-    alcohol_drinking = 1 if alcohol_drinking=='Yes' else 0
-    stroke = 1 if stroke=='Yes' else 0
-    diff_walking = 1 if diff_walking=='Yes' else 0
-    sex = 1 if sex=='Male' else 0
-    physical_activity = 1 if physical_activity=='Yes' else 0
-    asthma = 1 if asthma=='Yes' else 0
-    kidney_disease = 1 if kidney_disease=='Yes' else 0
-    skin_cancer = 1 if skin_cancer=='Yes' else 0
+    smoking = 1.0 if smoking=='Yes' else 0.0
+    alcohol_drinking = 1.0 if alcohol_drinking=='Yes' else 0.0
+    stroke = 1.0 if stroke=='Yes' else 0.0
+    diff_walking = 1.0 if diff_walking=='Yes' else 0.0
+    sex = 1.0 if sex=='Male' else 0.0
+    physical_activity = 1.0 if physical_activity=='Yes' else 0.0
+    asthma = 1 if asthma=='Yes' else 0.0
+    kidney_disease = 1.0 if kidney_disease=='Yes' else 0.0
+    skin_cancer = 1 if skin_cancer=='Yes' else 0.0
     
-    age_category_18_24 = 1 if age_category == '18-24' else 0
-    age_category_25_29 = 1 if age_category == '25-29' else 0
-    age_category_30_34 = 1 if age_category == '30-34' else 0 
-    age_category_35_39 = 1 if age_category == '18-24' else 0
-    age_category_40_44 = 1 if age_category == '40-44' else 0
-    age_category_45_49 = 1 if age_category == '45-49' else 0
-    age_category_50_54 = 1 if age_category == '50-54' else 0  
-    age_category_55_59 = 1 if age_category == '55-59' else 0 
-    age_category_60_64 = 1 if age_category == '60-64' else 0 
-    age_category_65_69 = 1 if age_category == '65-69' else 0 
-    age_category_70_74 = 1 if age_category == '70-74' else 0
-    age_category_75_79 = 1 if age_category == '75-79' else 0
-    age_category_80_or_older = 1 if age_category == '80 or older' else 0
+    age_category_18_24 = 1.0 if age_category == '18-24' else 0.0
+    age_category_25_29 = 1.0 if age_category == '25-29' else 0.0
+    age_category_30_34 = 1.0 if age_category == '30-34' else 0.0 
+    age_category_35_39 = 1.0 if age_category == '18-24' else 0.0
+    age_category_40_44 = 1.0 if age_category == '40-44' else 0.0
+    age_category_45_49 = 1.0 if age_category == '45-49' else 0.0
+    age_category_50_54 = 1.0 if age_category == '50-54' else 0.0  
+    age_category_55_59 = 1 if age_category == '55-59' else 0.0 
+    age_category_60_64 = 1.0 if age_category == '60-64' else 0.0
+    age_category_65_69 = 1.0 if age_category == '65-69' else 0.0
+    age_category_70_74 = 1.0 if age_category == '70-74' else 0.0
+    age_category_75_79 = 1.0 if age_category == '75-79' else 0.0
+    age_category_80_or_older = 1.0 if age_category == '80 or older' else 0.0
     
 
-    race_aia_native = 1 if race == 'American Indian/Alaskan Native' else 0
-    race_asian = 1 if race == 'Asian' else 0
-    race_black = 1 if race == 'Black' else 0
-    race_hispanic = 1 if race == 'Hispanic' else 0
-    race_other = 1 if race == 'Other' else 0
-    race_white = 1 if race == 'White' else 0
+    race_aia_native = 1.0 if race == 'American Indian/Alaskan Native' else 0.0
+    race_asian = 1.0 if race == 'Asian' else 0.0
+    race_black = 1.0 if race == 'Black' else 0.0
+    race_hispanic = 1.0 if race == 'Hispanic' else 0.0
+    race_other = 1.0 if race == 'Other' else 0.0
+    race_white = 1.0 if race == 'White' else 0.0
 
-    gen_health_excellent = 1 if genHealth == 'Excellent' else 0
-    gen_health_fair = 1 if genHealth == 'Fair' else 0
-    gen_health_good = 1 if genHealth == 'Good' else 0
-    gen_health_poor = 1 if genHealth == 'Poor' else 0 
-    gen_health_very_good = 1 if genHealth == 'Very good' else 0
+    gen_health_excellent = 1.0 if genHealth == 'Excellent' else 0.0
+    gen_health_fair = 1.0 if genHealth == 'Fair' else 0.0
+    gen_health_good = 1.0 if genHealth == 'Good' else 0.0
+    gen_health_poor = 1.0 if genHealth == 'Poor' else 0.0 
+    gen_health_very_good = 1.0 if genHealth == 'Very good' else 0.0
 
-    diabetic_no = 1 if diabetic == 'No' else 0
-    diabetic_no_bd = 1 if diabetic == 'No, borderline diabetes' else 0
-    diabetic_yes = 1 if diabetic == 'Yes' else 0
-    diabetic_yes_dp = 1 if diabetic == 'Yes (during pregnancy)' else 0
+    diabetic_no = 1.0 if diabetic == 'No' else 0.0
+    diabetic_no_bd = 1.0 if diabetic == 'No, borderline diabetes' else 0.0
+    diabetic_yes = 1.0 if diabetic == 'Yes' else 0.0
+    diabetic_yes_dp = 1.0 if diabetic == 'Yes (during pregnancy)' else 0.0
 
 
     # Prepare the input for prediction. This has to go in the same order as it was trained
@@ -126,12 +129,15 @@ if submitted:
                             diabetic_yes_dp
                         ]]
 
+
+    
     # Make prediction
+    print(prediction_input)
     new_prediction = rf_model.predict(prediction_input)
 
     # Display result
     st.subheader("Prediction Result:")
-    if new_prediction[0] == '1':
+    if new_prediction[0] == 1:
         st.write("You may have heart disease.")
     else:
         st.write("You may not have heart disease.")
